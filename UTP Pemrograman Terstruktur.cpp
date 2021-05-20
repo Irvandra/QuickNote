b7fedc949c76e74f76c2dc5ebc2be06e5141a63a
Irvandra <irvandra.apps@gmail.com>
2021-05-20 10:26:00 +0700

    Create UTP Pemrograman Terstruktur.cpp

---
+++ UTP Pemrograman Terstruktur.cpp
@@ -0,0 +1,180 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class MyNote{
+public:
+    string note[100];
+    string judul;
+    int totalNote = 0;
+    MyNote *nextNote;
+
+    MyNote(string _judul){
+        judul = _judul;
+    }
+    
+    void SaveNote(string _text){
+        note[totalNote] = _text;
+        totalNote++;
+	}
+
+    void ShowNote(){
+        for (int i = 0; i < totalNote; i++)
+        {
+            cout << "------------------------------" << judul << "------------------------------" << endl << endl;
+            cout << note[i] << endl;
+        }        
+    }
+};
+
+MyNote *myHeadNote;
+
+void init(){
+    myHeadNote = NULL;
+}
+
+bool IsEmpty(){
+    if(myHeadNote == NULL){
+        return true;
+    }
+
+    return false;
+}
+
+void BukaNote(string _judul){
+    MyNote *myNote;
+    int totalNote;
+
+    myNote = myHeadNote;
+
+    while (myNote -> judul != _judul)
+    {
+        myNote = myNote -> nextNote;
+    }
+    
+    if(myNote -> judul == _judul){
+        cout << "------------------------------" << myNote -> judul << "------------------------------" << endl << endl;
+
+        totalNote = myNote -> totalNote;
+
+        for (int i = 0; i < totalNote; i++)
+        {
+            cout << myNote -> note[i] << endl;
+        }
+    }
+}
+
+void HapusNote(){
+    cout << "Terhapus" << endl;
+}
+
+void TulisNote(){
+    cout << "------------------------------My Note------------------------------" << endl << endl;
+    cout << "Maks 100 baris" << endl;
+    cout << "Tulis 'END' dibaris terakhir untuk menyimpan!" << endl << endl;
+
+    MyNote *myNote, *findLastNote;
+    string note[100];
+    string judul, text;
+    int totalNote = 0;
+    int pilihan;
+
+    cout << "Judul Note : "; cin >> judul;
+    cout << "My Note : " << endl;
+    
+    cin.ignore();
+    myNote = new MyNote(judul);
+    
+    for (int i = 0; i < 100; i++)
+    {
+        getline(cin, text);
+        
+        note[i] = text;
+        myNote -> SaveNote(text);
+        totalNote = i;
+
+        if(i == 98){
+            note[i] = "END";
+            cout << "END" << endl;
+        }else if(text == "END"){
+            i = 100;
+        }
+    }
+    
+    myNote -> nextNote = NULL;
+
+    if(IsEmpty()){
+        myHeadNote = myNote;
+        myHeadNote -> nextNote = NULL;    
+    }else{
+        findLastNote = myHeadNote;
+
+        while (findLastNote -> nextNote != NULL)
+        {
+            findLastNote = findLastNote -> nextNote;
+        }
+        
+        findLastNote -> nextNote = myNote;    
+    }
+
+    cout << "Note tersimpan!" << endl;
+    cin.get();
+    system("CLS");
+    
+    myNote -> ShowNote();
+
+    cout << endl;
+    cout << "1. Hapus Note" << endl;
+    cout << "2. Kembali ke Menu" << endl;
+    cout << "Pilihan anda : "; cin >> pilihan;
+
+    if(pilihan == 1){
+        HapusNote();
+    }
+}
+
+void BukaNoteList(){
+    cout << "------------------------------Note List------------------------------" << endl << endl;
+}
+
+void CariNote(){
+    cout << "------------------------------Find Note------------------------------" << endl << endl;
+}
+
+int main(){
+    int pilihan;
+
+    do
+    {
+        system("CLS");
+
+	    cout << "------------------------------Quick Note------------------------------" << endl << endl;
+        cout << "0. Keluar" << endl;
+        cout << "1. Tulis Note" << endl;
+        cout << "2. Buka Note List" << endl;
+        cout << "3. Cari Note" << endl;
+
+        cout << "Pilihan anda : "; cin >> pilihan;
+        system("CLS");
+
+        switch (pilihan)
+        {
+            case 1:
+                TulisNote();
+                break;
+            
+            case 2:
+                BukaNoteList();
+                break;
+                
+            case 3:
+                CariNote();
+                break;
+                
+            default:
+                return 0;
+                break;
+        }
+    } while (pilihan == 1 || pilihan == 2 || pilihan == 3);    
+}
